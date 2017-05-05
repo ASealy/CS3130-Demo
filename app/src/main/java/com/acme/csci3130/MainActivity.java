3e0affb6d53c3ef9fc68ae7b304c1a166820823d
Andrew Sealy <andrewjsealy@live.ca>
2017-05-05 16:30:20 -0300

    New UI elements...

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,9 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void event(View view) {
+        EditText editText = (EditText) findViewById(R.id.input);
+        String message = editText.getText().toString();
+        TextView textView = (TextView) findViewById(R.id.display);
+        textView.setText(message);
     }
 }
